59730d9012d9eb0471679a62efaf2ec3f3658c57
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-04-18 16:13:11 +0300

    Update cmain.cpp

--- cmain.cpp
+++ cmain.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <assert.h>
 #include <time.h>
+#include <iomanip>
 
 using namespace std;
 const double PI = acos(-1);
@@ -21,7 +22,7 @@ int mod_pow(int base, int exp, int m) {
     return result;
 }
 
-void ntt(vector<int>& a, bool invert) {
+void ntt(vector<int>& a, bool invert, bool other) {
     int n = a.size();
     int root_pw = mod_pow(root, (mod - 1) / n, mod);
     if (invert) root_pw = mod_pow(root_pw, mod - 2, mod); // inverse root
@@ -38,18 +39,38 @@ void ntt(vector<int>& a, bool invert) {
             //cout << i << " " << j << endl;
         }
     }
-
-    for (int len = 2; len <= n; len <<= 1) {
-        int wlen = mod_pow(root, (mod - 1) / len, mod);
-        if (invert) wlen = mod_pow(wlen, mod - 2, mod);
-        for (int i = 0; i < n; i += len) {
-            int w = 1;
-            for (int j = 0; j < len / 2; ++j) {
-                int u = a[i + j];
-                int v = (1LL * a[i + j + len / 2] * w) % mod;
-                a[i + j] = (u + v) % mod;
-                a[i + j + len / 2] = (u - v + mod) % mod;
-                w = (1LL * w * wlen) % mod;
+    if (other)
+    {
+        for (int i = 1; i <= (int)log2(n); i++) {
+            int lencopy = (int)pow(2, i);
+            int wlen = mod_pow(root, (mod - 1) / lencopy, mod);
+            if (invert) wlen = mod_pow(wlen, mod - 2, mod);
+            for (int j = 0; j < n / lencopy; j++) {
+                int w = 1;
+                for (int k = 0; k < lencopy / 2; k++) {
+                    int u = a[(j * lencopy) + k];
+                    int v = (1LL * a[(j * lencopy)+ k + lencopy / 2] * w) % mod;
+                    a[(j * lencopy)+ k] = (u + v) % mod;
+                    a[(j * lencopy)+ k + lencopy / 2] = (u - v + mod) % mod;
+                    w = (1LL * w * wlen) % mod;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int len = 2; len <= n; len <<= 1) {
+            int wlen = mod_pow(root, (mod - 1) / len, mod);
+            if (invert) wlen = mod_pow(wlen, mod - 2, mod);
+            for (int i = 0; i < n; i += len) {
+                int w = 1;
+                for (int j = 0; j < len / 2; ++j) {
+                    int u = a[i + j];
+                    int v = (1LL * a[i + j + len / 2] * w) % mod;
+                    a[i + j] = (u + v) % mod;
+                    a[i + j + len / 2] = (u - v + mod) % mod;
+                    w = (1LL * w * wlen) % mod;
+                }
             }
         }
     }
@@ -115,18 +136,18 @@ vector<complex<double>> FFTMultiply(const vector<complex<double>>& a, const vect
     return fa;
 }
 
-vector<int> NTTMultiply(vector<int> const& a, vector<int> const& b) {
+vector<int> NTTMultiply(vector<int> const& a, vector<int> const& b, bool other) {
     vector<int> fa(a.begin(), a.end()), fb(b.begin(), b.end());
     int n = 1;
     while (n < a.size() + b.size()) n <<= 1;
     fa.resize(n);
     fb.resize(n);
 
-    ntt(fa, false);
-    ntt(fb, false);
+    ntt(fa, false, other);
+    ntt(fb, false, other);
     for (int i = 0; i < n; i++)
         fa[i] = (1LL * fa[i] * fb[i]) % mod;
-    ntt(fa, true);
+    ntt(fa, true, other);
 
     return fa;
 }
@@ -153,66 +174,59 @@ void DisplayVector(vector<T> vec)
     cout << endl;
 }
 
-void FillVectorRandom(vector<int> vec, int a, int b)
+void FillVectorRandom(vector<int>& vec, int a, int b)
 {
     for(int i = 0; i < vec.size(); i++)
         vec[i] = a + (rand() % (b - a + 1));
 }
 
-
-int n = 8;
-void GoldenFoo()
+template<class T>
+void CompareVectors(vector<T>& c, vector<T>& cother)
 {
-    for (int i = 2; i <= n; i <<= 1) {
-        cout << "index i = " << i << "\n";
-        for (int j = 0; j < n; j += i) {
-            cout << "\tindex j = " << j << "\n";
-            for (int k = 0; k < i / 2; k++) {
-                cout << "\t\tindex k = " << k << "\n";
-            }
-        }
-    }
-}
-
-void foo()
-{
-    for (int i = 1; i <= (int)log2(n); i++) {
-        cout << "index i = " << (int)pow(2, i) << "\n";
-        int icopy = (int)pow(2, i);
-        for (int j = 0; j < n / icopy; j++) {
-            cout << "\tindex j = " << j * icopy << "\n";
-            for (int k = 0; k < icopy / 2; k++) {
-                cout << "\t\tindex k = " << k << "\n";
-            }
+    assert(c.size() == cother.size());
+    for (int i = 0; i < c.size(); i++)
+    {
+        if (c[i] != cother[i])
+        {
+            cout << "difference found\n";
+            exit(EXIT_FAILURE);
         }
     }
 }
 
 
+clock_t clocker;
+void StartClock()
+{
+    clocker = clock();
+}
+void EvaluateClock(string Message)
+{
+    clock_t t = clock() - clocker;
+    cout << Message << "\n";
+    cout << std::fixed << std::setprecision(3) << "Time taken: " << (double)(t) / CLOCKS_PER_SEC << "s\n";
+}
 int main()
 {
-    GoldenFoo();
-    cout << "---------------------------------------------------------------------------------------------------------------------------------------\n";
-    foo();
-    return 0;
-    /*
-    1 2 4 1 3 
-    1 4 3 2 3 
-    1 6 15 25 26 29 23 9 9 0 0 0 0 0 0 0
-    */
-    int n = 5;
+    int n = 10 * 1024 * 1024;
     srand(time(0));
-    vector<int> a(n);
-    a = {1, 2, 4, 1, 3};
-    vector<int> b(n);
-    b = {1, 4, 3, 2, 3};
 
-    clock_t tStart = clock();
-    vector<int> c = NTTMultiply(a, b);
-    cout << "done\n";
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
-    DisplayVector(a);
-    DisplayVector(b);
-    DisplayVector(c);
+    vector<int> a(n);
+    FillVectorRandom(a, 1, 5);
+    vector<int> b(n);
+    FillVectorRandom(b, 1, 5);
+
+    vector<int> c;
+
+    StartClock();
+        c = NTTMultiply(a, b, false);
+    EvaluateClock("the origianl");
+
+    c.clear();
+
+    StartClock();
+        c = NTTMultiply(a, b, true);
+    EvaluateClock("the new one with indexing");
+
     return 0;
 }
