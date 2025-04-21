ed3124089f8eea757509bb21d752a7c613e72d82
Nedal Abu Haltam <nedalhaltam1@gmail.com>
2025-04-21 18:32:05 +0300

    update

--- cmain.cpp
+++ cmain.cpp
@@ -8,20 +8,82 @@
 #include <time.h>
 #include <iomanip>
 
-#include "nttlib.h"
-
 using namespace std;
 
 
+int mod_pow(int base, int exp, int mod) {
+    int result = 1;
+    base %= mod;
+    while (exp > 0) {
+        if (exp & 1)
+            result = (1LL * result * base) % mod;
+        base = (1LL * base * base) % mod;
+        exp >>= 1;
+    }
+    return result;
+}
+
+template<class T>
+void DisplayVector(vector<T> vec)
+{
+    cout << "[";
+    for (int i = 0; i < vec.size() - 1; i++)
+    {
+        cout << vec[i] << ", ";
+    }
+    cout << vec[vec.size() - 1];
+    cout << "]\n";
+}
+
+template<class T>
+void FillVectorRandom(vector<T>& vec, T a, T b, T (*GetRandomT)(T, T))
+{
+    for(int i = 0; i < vec.size(); i++)
+        vec[i] = GetRandomT(a, b);
+}
+
+template<class T>
+void CompareVectors(vector<T>& c, vector<T>& cother)
+{
+    assert(c.size() == cother.size());
+    for (int i = 0; i < c.size(); i++)
+    {
+        if (c[i] != cother[i])
+        {
+            cout << "difference found\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
 
+clock_t clocker;
+void StartClock()
+{
+    clocker = clock();
+}
+void EvaluateClock(string Message)
+{
+    clock_t t = clock() - clocker;
+    cout << Message << "\n";
+    cout << std::fixed << std::setprecision(3) << "Time taken: " << (double)(t) / CLOCKS_PER_SEC << "s\n";
+}
+
+int GetRandomInt(int a, int b)
+{
+    return a + (rand() % (b - a + 1));
+}
+
+complex<double> GetRandomComplex(complex<double> a, complex<double> b)
+{
+    complex<double> t(b - a + complex<double>(1, 1));
+    return complex<double>(a.real() + rand() % (int)t.real(), a.imag() + rand() % (int)t.imag());
+}
 
 
-
-vector<int> ntt_naive(const vector<int>& a, int root, int mod, bool invert = false) {
+vector<int> ntt_naive(vector<int>& a, int root, int mod, bool invert = false) {
     int n = a.size();
     vector<int> A(n, 0);
-    int sign = (invert) ? -1 : 1;
     root = (invert) ? mod_pow(root, mod - 2, mod) : root;
     for (int k = 0; k < n; ++k) {
         for (int j = 0; j < n; ++j) {
@@ -41,27 +103,34 @@ vector<int> ntt_naive(const vector<int>& a, int root, int mod, bool invert = fal
     return A;
 }
 
+vector<int> NTTMultiplynaive(vector<int>& a, vector<int>& b, int root, int mod)
+{
+    vector<int> fa(a), fb(b);
+    int n = 1;
+    while (n < a.size() + b.size()) n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+    fa = ntt_naive(fa, root, mod, false);
+    fb = ntt_naive(fb, root, mod, false);
+    for (int i = 0; i < n; i++)
+    {
+        fa[i] = (1LL * fa[i] * fb[i]) % mod;
+    }
+    return ntt_naive(fa, root, mod, true);
+}
+
 int main()
 {
     srand(time(0));
+    vector<int> p1 = {1, 2, 3, 4};
+    vector<int> p2 = {1, 2, 3, 4};
 
-    // int n = 4;
-    // int PrimeMod = 7681;
-    // int ROOT = 3383;
-    int n = 8;
-    int PrimeMod = 3263993;
-    int ROOT = 1032801;
+    int mod = 3278753;
+    int root = 449739;
 
-    // vector<int> a = {1, 2, 3, 4};
-    vector<int> a = {1, 2, 3, 4, 5, 6, 7, 8};
-    // FillVectorRandom(a, 1, 5, GetRandomInt);
-
-    DisplayVector(a);
-    vector<int> c = ntt_naive(a, ROOT, PrimeMod);
+    vector<int> c = NTTMultiplynaive(p1, p2, root, mod);
     DisplayVector(c);
-    vector<int> c2 = ntt_naive(c, ROOT, PrimeMod, true);
-    DisplayVector(c2);
-    
+
     return 0;
 }
 
--- nthroot-generator.py
+++ nthroot-generator.py
@@ -1,35 +1,36 @@
 import sympy
 
-n = 4
-index = 974
-# n = 8
-# index = 234511
-PrimeNumber = sympy.prime(index)
-while (PrimeNumber - 1) % n != 0:
-    index += 1
+def GenerateNTT(n : int, index : int):
     PrimeNumber = sympy.prime(index)
-roots = sympy.nthroot_mod(1, n, PrimeNumber, all_roots=True)
+    while not ((PrimeNumber - 1) % n == 0 and PrimeNumber % (2 * n) == 1):
+        index += 1
+        PrimeNumber = sympy.prime(index)
+    roots = sympy.nthroot_mod(1, n, PrimeNumber, all_roots=True)
 
-finals = []
-print(roots)
-for root in roots:
-    foo = True
-    for i in range(1, n, 1):
-        if root ** i % PrimeNumber == 1:
-            foo = False
-            break
-    if foo:
-        finals.append(root)
+    finals = []
+    # print(roots)
+    for root in roots:
+        foo = True
+        for i in range(1, n, 1):
+            if root ** i % PrimeNumber == 1:
+                foo = False
+                break
+        if foo:
+            finals.append(root)
 
-print(f'The prime number is : {PrimeNumber}')
-print(f'the possible nth roots of unity are {finals}')
+    print(f'The prime number is : {PrimeNumber}')
+    print(f'the possible nth roots of unity are {finals}')
+
+    # sequence  
+    seq = range(1, n + 1)
+    prime_no = PrimeNumber
+    # ntt 
+    transform = sympy.ntt(seq, prime_no) 
+    invtransform = sympy.intt(transform, prime_no)
+    print("NTT :",transform) 
+    # print ("inverse NTT :",invtransform) 
+    print('----------------------------------------------------------------------------------------------------------------')
 
 
-# sequence  
-seq = range(1, 5) 
-prime_no = PrimeNumber
-# ntt 
-transform = sympy.ntt(seq, prime_no) 
-invtransform = sympy.intt(transform, prime_no)
-print ("NTT : ", transform) 
-print ("inverse NTT : ", invtransform) 
+GenerateNTT(8, 235511)
+
